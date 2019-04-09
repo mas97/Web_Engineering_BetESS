3b961a6355e9f5dbe54e5317d8ecc5c86b2a3c94
Diana Costa <dianasofia.costa@sapo.pt>
2019-04-09 12:07:58 +0100

    updated user site view & added stored procedures to add/subtract balance

---
+++ mySQL/stored procedures/subt_balance.sql
@@ -0,0 +1,34 @@
+DROP PROCEDURE IF EXISTS subt_balance;
+DELIMITER $$
+CREATE PROCEDURE subt_balance(IN amount DECIMAL,
+							  IN id_user INT)
+BEGIN
+	DECLARE erro BOOL DEFAULT 0;
+    DECLARE aux DECIMAL DEFAULT 0;
+	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET erro = 1;
+    START TRANSACTION;
+
+    SET aux = (SELECT (user.balance - amount) 
+			   FROM user 
+			   WHERE user.oid = id_user
+               );
+	
+	IF (aux >= 0) 
+		THEN UPDATE user SET user.balance = (SELECT (balance - amount) WHERE oid = id_user);
+    END IF;
+    
+    
+	IF erro 
+		THEN ROLLBACK;
+		ELSE COMMIT;
+	END IF;
+END
+$$
+DELIMITER ;
+
+
+-- TEST1: SUBTRACT 5c TO USER WITH OID=1 TO SIMULATE DRAW
+-- TEST WITH VALUE GREATER THAN BALANCE TO CHECK THAT BALANCE DOES NOT CHANGE
+CALL subt_balance(12, 1);
+
+ SELECT * FROM user;
---
+++ mySQL/stored procedures/sum_balance.sql
@@ -0,0 +1,25 @@
+DROP PROCEDURE IF EXISTS sum_balance;
+DELIMITER $$
+CREATE PROCEDURE sum_balance(IN amount DECIMAL,
+							 IN id_user INT)
+BEGIN
+	DECLARE erro BOOL DEFAULT 0;
+	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET erro = 1;
+	START TRANSACTION;
+
+	UPDATE user SET user.balance = (SELECT(balance + amount)) WHERE user.oid = id_user;
+
+	IF erro 
+		THEN ROLLBACK;
+		ELSE COMMIT;
+	END IF;
+END
+$$
+DELIMITER ;
+
+-- TEST: ADD MORE 5c TO USER WITH OID=1
+ CALL sum_balance(5, 1);
+ SELECT * FROM user;
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE user SET user.balance = 5;
