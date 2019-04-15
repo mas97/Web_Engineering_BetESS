f87d5d5879fc07840c1bb2689236198f2d6c78c7
Diana Costa <dianasofia.costa@sapo.pt>
2019-04-15 18:35:44 +0100

    improved notifications' area & added cashout procedure
    user site view almost finished

---
+++ mySQL/stored procedures/cashout.sql
@@ -0,0 +1,31 @@
+DROP PROCEDURE IF EXISTS cashout;
+DELIMITER $$
+CREATE PROCEDURE cashout(IN id_bet INT, IN id_user INT)
+BEGIN
+	DECLARE erro BOOL DEFAULT 0;
+    DECLARE aux DECIMAL DEFAULT 0;
+	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET erro = 1;
+	START TRANSACTION;
+
+	SELECT (bet.amount DIV 2) INTO aux
+    FROM bet
+    WHERE bet.oid = id_bet; 
+
+    
+	UPDATE user SET user.balance = (user.balance + aux) WHERE user.oid = id_user;
+    
+    DELETE FROM bet WHERE oid = id_bet;
+
+	IF erro 
+		THEN ROLLBACK;
+		ELSE COMMIT;
+	END IF;
+END
+$$
+DELIMITER ;
+
+
+-- TEST
+ CALL cashout(2, 2);
+ SELECT * FROM user;
+ SELECT * FROM bet;
