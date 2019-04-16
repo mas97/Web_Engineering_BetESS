246c307e7c575e5ada35ef49cb2fa5380a0dfcfa
Marco Silva <marant_silva@hotmail.com>
2019-04-16 11:43:11 +0100

    premium procedure added and not tested/ still missing premium user elements

---
+++ mySQL/stored procedures/premium.sql
@@ -0,0 +1,27 @@
+DROP PROCEDURE IF EXISTS premium;
+DELIMITER $$
+CREATE PROCEDURE premium(IN id_user INT)
+BEGIN
+	DECLARE erro BOOL DEFAULT 0;
+    DECLARE aux DECIMAL DEFAULT 0;
+	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET erro = 1;
+    START TRANSACTION;
+
+	SELECT (user.balance - 10) INTO aux
+    FROM user
+    WHERE user.oid = id_user;
+	
+    
+	IF aux >= 0 THEN
+		UPDATE user SET balance = aux WHERE user.oid = id_user;
+		UPDATE user SET premium = 1 WHERE user.oid = id_user;
+    END IF;
+    
+    
+	IF erro 
+		THEN ROLLBACK;
+		ELSE COMMIT;
+	END IF;
+END
+$$
+DELIMITER ;
--- mySQL/stored procedures/queries.sql
+++ mySQL/stored procedures/queries.sql
@@ -1,25 +1,21 @@
 -- query "Available Events"
 
 SELECT CONCAT("Event nº",e.oid), 
-	   GROUP_CONCAT(t.name), e.oddhome, 
-       e.oddaway, e.odddraw, l.name, 
+	   (SELECT name FROM team t where e.team_oid = t.oid) AS 'Home Team', (SELECT name FROM team t where e.team_oid_2 = t.oid) AS 'Away Team',
+       e.oddhome, e.oddaway, e.odddraw, l.name, 
        s.name
 FROM Event e
 INNER JOIN League l ON e.league_oid = l.oid
 INNER JOIN Sport s ON e.sport_oid = s.oid
-INNER JOIN Event_Team et ON e.oid = et.event_oid
-INNER JOIN Team t ON et.team_oid = t.oid
 GROUP BY(e.oid);
 
 
 -- query "My Bets"
 
 SELECT b.oid, CONCAT("Event nº",e.oid), 
-	   GROUP_CONCAT(t.name),
+	   (SELECT name FROM team t where e.team_oid = t.oid) AS 'Home Team', (SELECT name FROM team t where e.team_oid_2 = t.oid) AS 'Away Team',
        b.amount, b.result
 FROM Event e
-INNER JOIN Event_Team et ON e.oid = et.event_oid
-INNER JOIN Team t ON et.team_oid = t.oid
 INNER JOIN Bet b ON e.oid = b.event_oid
 GROUP BY b.oid;
 
@@ -40,10 +36,10 @@ GROUP BY b.oid;
 
 -- query "My Notifications"
 
-SELECT b.oid AS betNumber, n.event_oid, e.result, n.balancebet, n.status 
+SELECT b.oid AS betNumber,
+	(SELECT name FROM team t where e.team_oid = t.oid) AS 'Home Team', (SELECT name FROM team t where e.team_oid_2 = t.oid) AS 'Away Team',
+	n.event_oid, e.result, n.balancebet, n.status 
 FROM Notification n
 INNER JOIN Event e ON n.event_oid = e.oid
 INNER JOIN Bet b ON e.oid = b.event_oid
-INNER JOIN Event_Team et ON e.oid = et.event_oid
-INNER JOIN Team t ON et.team_oid = t.oid
 GROUP BY n.oid;
