4d9a5b8174e8a14c5ef6785dd4011d6fe9700991
Marco Silva <marant_silva@hotmail.com>
2019-04-18 11:57:02 +0100

    added some events and teams for future demo of the platform

--- mySQL/povoamento.sql
+++ mySQL/povoamento.sql
@@ -23,3 +23,19 @@ CHANGE COLUMN `oid` `oid` INT(11) NOT NULL AUTO_INCREMENT ;
 
 ALTER TABLE `betess`.`notification` 
 CHANGE COLUMN `oid` `oid` INT(11) NOT NULL AUTO_INCREMENT ;
+
+INSERT INTO `betess`.`team` (`oid`, `name`) VALUES ('1', 'Fc Porto');
+INSERT INTO `betess`.`team` (`oid`, `name`) VALUES ('2', 'SL Benfica');
+INSERT INTO `betess`.`team` (`oid`, `name`) VALUES ('3', 'Tondela');
+INSERT INTO `betess`.`team` (`oid`, `name`) VALUES ('4', 'Belenenses SAD');
+INSERT INTO `betess`.`team` (`oid`, `name`) VALUES ('5', 'Boavista');
+INSERT INTO `betess`.`team` (`oid`, `name`) VALUES ('6', 'Marítimo');
+INSERT INTO `betess`.`team` (`oid`, `name`) VALUES ('7', 'Rio Ave FC');
+INSERT INTO `betess`.`team` (`oid`, `name`) VALUES ('8', 'Portimonense');
+INSERT INTO `betess`.`team` (`oid`, `name`) VALUES ('9', 'Sporting');
+INSERT INTO `betess`.`team` (`oid`, `name`) VALUES ('10', 'Sp. Braga');
+
+INSERT INTO `betess`.`event` (`oid`, `odddraw`, `oddaway`, `oddhome`, `result`, `status`, `premium`, `sport_oid`, `team_oid`, `league_oid`, `team_oid_2`) VALUES ('1', '1.27', '5.20', '9.50', 'NA', 'Open', TRUE, '1', '3', '1', '7');
+INSERT INTO `betess`.`event` (`oid`, `odddraw`, `oddaway`, `oddhome`, `result`, `status`, `premium`, `sport_oid`, `team_oid`, `league_oid`, `team_oid_2`) VALUES ('2', '1.71', '3.85', '4.0', 'NA', 'Open', FALSE, '1', '5', '1', '2');
+INSERT INTO `betess`.`event` (`oid`, `odddraw`, `oddaway`, `oddhome`, `result`, `status`, `premium`, `sport_oid`, `team_oid`, `league_oid`, `team_oid_2`) VALUES ('3', '2', '1.7', '3.6', 'NA', 'Open', FALSE, '1', '9', '1', '8');
+INSERT INTO `betess`.`event` (`oid`, `odddraw`, `oddaway`, `oddhome`, `result`, `status`, `premium`, `sport_oid`, `team_oid`, `league_oid`, `team_oid_2`) VALUES ('4', '2.2', '3.7', '5.6', 'NA', 'OPEN', TRUE, '1', '4', '1', '6');
