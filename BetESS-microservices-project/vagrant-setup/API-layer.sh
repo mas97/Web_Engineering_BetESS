e4b197563bdf85d0bdb35e4a897f300414ac3f27
Marco Silva <marant_silva@hotmail.com>
2019-05-21 21:16:15 +0100

    nginx 1st configuration and new vagrant provision file

--- BetESS-microservices-project/vagrant-setup/API-layer.sh
+++ BetESS-microservices-project/vagrant-setup/API-layer.sh
@@ -11,4 +11,14 @@ apt-get install -y git
 
 # nginx installation
 apt-get install -y nginx
-service nginx start
+
+# remove default configuration
+sudo rm /etc/nginx/sites-available/default
+sudo rm /etc/nginx/sites-enabled/default
+
+cp /home/vagrant/files/reverse-proxy.conf /etc/nginx/sites-available
+
+ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
+
+service nginx configtest
+service nginx restart
