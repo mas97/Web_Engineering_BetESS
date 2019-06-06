4bf719122c8ca015444b5cd36b74e1da9b5a7804
Diana Costa <dianasofia.costa@sapo.pt>
2019-06-06 19:51:49 +0100

    started front end user page

--- BetESS-frontend/src/components/Admin.vue
+++ BetESS-frontend/src/components/Admin.vue
@@ -1,35 +1,27 @@
 <template>
-	<h3>Página do admin</h3>
+
+<div> 
+ <ul>
+  <li><a href="default.asp">Home</a></li>
+  <li><a href="news.asp">News</a></li>
+  <li><a href="contact.asp">Contact</a></li>
+  <li><a href="about.asp">About</a></li>
+</ul> 
+</div>
+
 </template>
 
 <script>
 export default {
   name: 'Admin',
-  props: {
 
-  }
 }
+
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-
-img {
-  width: 250px;
-}
 
 </style>
