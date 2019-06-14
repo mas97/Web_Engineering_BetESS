1fb32037a08bc4cee14853bbf76181a16552263e
Diana Costa <dianasofia.costa@sapo.pt>
2019-06-14 16:30:46 +0100

    Update Credits.vue
    missing buttons

--- BetESS-frontend/src/components/Credits.vue
+++ BetESS-frontend/src/components/Credits.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <NavbarToOffcanvas v-if="['calendario'].indexOf($route.name) < 0" ></NavbarToOffcanvas>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <div class="container-full">
       
       <h3 style="text-align: left;"> Credits Management </h3>
@@ -16,8 +17,8 @@
                 <label>Insert the amount to draw:</label>
               </div>
             </div>
-          <br/>
-          <h6 style="color:gray;"><small>* The amount will be automatically returned to your bank account.</small></h6>
+          <h6 style="color:gray;"><small>* The amount will be returned to your bank account.</small></h6>
+          <button class="btn btn-submit" type="submit" style="float:right; margin-bottom:5px; margin-right:-10px; margin-top: 15px;">Submit</button>
           </div>
 
 
@@ -30,8 +31,8 @@
                 <label>Insert the amount to deposit:</label>
               </div>
             </div>
-          <br/>
-          <h6 style="color:gray;"><small>* The amount will be automatically withdrawn from your bank account.</small></h6>
+          <h6 style="color:gray;"><small>* The amount will be withdrawn from your bank account.</small></h6>
+          <button class="btn btn-submit" type="submit" style="float:right; margin-bottom:5px; margin-right:-10px; margin-top: 15px;">Submit</button>
           </div> 
         
         </div>
@@ -62,6 +63,56 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.btn {
+  background: #fff;
+  color: #959595;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 3px;
+  letter-spacing: 0.06em;
+  text-decoration: none;
+  outline: none;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+}
+.btn:hover {
+  color: #8b8b8b;
+  box-shadow: 0 7px 14px rgba(0, 0, 0, 0.18), 0 5px 5px rgba(0, 0, 0, 0.12);
+}
+.btn.btn-link {
+  background: #2196F3;
+  color: #d3eafd;
+}
+.btn.btn-link:hover {
+  background: #0d8aee;
+  color: #deeffd;
+}
+.btn.btn-submit {
+  background: white;
+  color: orange;
+  border: solid orange; 
+  border-width: 1px 1px 1px 1px;
+  float: right;
+}
+.btn.btn-submit:hover {
+  background: orange;
+  color: black;
+}
+.btn.btn-cancel {
+  background: #eee;
+}
+.btn.btn-cancel:hover {
+  background: #e1e1e1;
+  color: #8b8b8b;
+}
+
+.btn-box {
+  text-align: center;
+  margin: 50px 0;
+}
+
+
 
 .container-full { 
   background-color: black;
@@ -193,54 +244,5 @@ label {
   transition: 300ms ease all;
   left: 0%;
 }
-
-.btn {
-  background: #fff;
-  color: #959595;
-  border: none;
-  padding: 10px 20px;
-  border-radius: 3px;
-  letter-spacing: 0.06em;
-  text-decoration: none;
-  outline: none;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
-}
-.btn:hover {
-  color: #8b8b8b;
-  box-shadow: 0 7px 14px rgba(0, 0, 0, 0.18), 0 5px 5px rgba(0, 0, 0, 0.12);
-}
-.btn.btn-link {
-  background: #2196F3;
-  color: #d3eafd;
-}
-.btn.btn-link:hover {
-  background: #0d8aee;
-  color: #deeffd;
-}
-.btn.btn-submit {
-  background: black;
-  color: orange;
-  border: solid orange; 
-  border-width: 1px 1px 1px 1px;
-  float: right;
-}
-.btn.btn-submit:hover {
-  background: orange;
-  color: black;
-}
-.btn.btn-cancel {
-  background: #eee;
-}
-.btn.btn-cancel:hover {
-  background: #e1e1e1;
-  color: #8b8b8b;
-}
-
-.btn-box {
-  text-align: center;
-  margin: 50px 0;
-}
-
 
 </style>
