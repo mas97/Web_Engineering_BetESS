55943509a5cfc5e329d44b8d05ee9fca553b99ee
Diana Costa <dianasofia.costa@sapo.pt>
2019-06-17 02:23:56 +0100

    added route guards for URL protection

--- BetESS-frontend/src/router.js
+++ BetESS-frontend/src/router.js
@@ -19,72 +19,119 @@ import Events from '@/components/Events'
 
 Vue.use(Router)
 
+/*  
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+      if ($store.dispatch('login/getToken') == null) {
+          next({
+              path: '/signinup',
+              params: { nextUrl: to.fullPath }
+          })
+      } else {
+          let user = JSON.parse($store.dispatch('login/getUser'))
+          if(to.matched.some(record => record.meta.is_admin)) {
+              if(user.is_admin == 1){
+                  next()
+              }
+              else{
+                  next({ name: 'user'})
+              }
+          }else {
+              next()
+          }
+      }
+  } else if(to.matched.some(record => record.meta.guest)) {
+      if($store.dispatch('login/getToken') == null){
+          next()
+      }
+      else{
+          next({ name: 'user'})
+      }
+  }else {
+      next() 
+  }
+})
+*/
+
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'signinup',
-      component: Signinup
+      component: Signinup,
+      // meta: { guest: true }
     },
     {
       path: '/admin',
       name: 'admin',
-      component: Admin
+      component: Admin,
+      // meta: { requiresAuth: true, isAdmin: true }
     },
     {
       path: '/user',
       name: 'user',
-      component: User
+      component: User,
+      // meta: { requiresAuth: true }
     },
     {
       path: '/bet',
       name: 'bet',
-      component: Bet
+      component: Bet,
+      //meta: { requiresAuth: true }
     },
     {
       path: '/mybets',
       name: 'mybets',
-      component: Mybets
+      component: Mybets,
+      //meta: { requiresAuth: true }
     },
     {
       path: '/notifications',
       name: 'notifications',
-      component: Notifications
+      component: Notifications,
+      //meta: { requiresAuth: true }
     },
     {
       path: '/credits',
       name: 'credits',
-      component: Credits
+      component: Credits,
+      //meta: { requiresAuth: true }
     },
     {
       path: '/profile',
       name: 'profile',
-      component: Profile
+      component: Profile,
+      //meta: { requiresAuth: true }
     },
     {
       path: '/sports',
       name: 'sports',
-      component: Sports
+      component: Sports,
+      //meta: { requiresAuth: true, isAdmin: true }
     },
     {
       path: '/leagues',
       name: 'leagues',
-      component: Leagues
+      component: Leagues,
+      //meta: { requiresAuth: true, isAdmin: true }
     },
     {
       path: '/teams',
       name: 'teams',
-      component: Teams
+      component: Teams,
+      //meta: { requiresAuth: true, isAdmin: true }
     },
     {
       path: '/bets',
       name: 'bets',
-      component: Bets
+      component: Bets,
+      //meta: { requiresAuth: true, isAdmin: true }
     },
     {
       path: '/events',
       name: 'events',
-      component: Events
+      component: Events,
+      //meta: { requiresAuth: true, isAdmin: true }
     }
   ]
 })
